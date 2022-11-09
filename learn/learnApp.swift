5bfd2fb86a8c07506df4ee55ba3a2d98539f780b
晒晕的兔子 <gubu1986@126.com>
2022-11-09 20:08:34 +0800

    Initial Commit

---
+++ learn/learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnApp.swift
+//  learn
+//
+//  Created by 顾艳华 on 2022/11/9.
+//
+
+import SwiftUI
+
+@main
+struct learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
