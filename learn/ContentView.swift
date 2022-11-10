f1cf4be0cb40fb179e0cf19f6e1e6df7029b4e7b
晒晕的兔子 <gubu1986@126.com>
2022-11-10 17:59:49 +0800

    ok

--- learn/ContentView.swift
+++ learn/ContentView.swift
@@ -8,19 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    let e = ["😜", "😀", "😄", "😆", "🥹", "🥰"]
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 25).stroke(lineWidth: 3)
-                .padding(.vertical)
-            Text("Hello, world!1")
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
+                    ForEach(e, id: \.self){
+                        emo in CardView(c: emo).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+                .foregroundColor(.red)
+            }
+            Spacer()
+            HStack {
+                Button(action: {
+                    
+                }, label: {
+                    Text("Remove")
+                })
+                Spacer()
+                Button(action: {
+                    
+                }, label: {
+                    Text("Add")
+                })
+            }.padding(.horizontal)
+         
         }
-        .padding()
-        .foregroundColor(.red)
+        .padding(.horizontal)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
+
+struct CardView: View {
+    @State var isFaceup = true
+    var c = ""
+    var body: some View {
+        ZStack {
+            if isFaceup {
+                RoundedRectangle(cornerRadius: 25).fill().foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 25).stroke(lineWidth: 3)
+                    
+                Text(c)
+            } else {
+                RoundedRectangle(cornerRadius: 25).fill()
+                    
+            }
+        }
+        .onTapGesture {
+            isFaceup = !isFaceup
+        }
     }
 }
